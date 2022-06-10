d3122289b09d99687a3119c5ac570c4f661d067f
valentin8019 <107219820+valentin8019@users.noreply.github.com>
2022-06-10 10:59:44 -0300

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
 input.onButtonPressed(Button.A, function () {
     music.playMelody("B A F D G E C5 C ", 120)
+    basic.showString(" HOLA QUE TAL?")
 })
 input.onGesture(Gesture.Shake, function () {
     music.stopAllSounds()
 })
-basic.showString("HOLA QUE TAL?")
+input.onButtonPressed(Button.AB, function () {
+    music.playMelody("C C D C5 F A G C5 ", 158)
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Once)
+})
 basic.forever(function () {
     basic.showLeds(`
         . # . # .
